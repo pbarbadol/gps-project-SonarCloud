c26fac49213e8277a2816a286c3a0dc04bf549e4
alberto <adominguoii@alumnos.unex.es>
2023-11-22 02:15:37 +0100

    GA02-273 CU14 - Implementacion - Arquitecto Software Implementar componentes

--- app/src/main/java/com/unex/asee/ga02/beergo/database/UserDao.kt
+++ app/src/main/java/com/unex/asee/ga02/beergo/database/UserDao.kt
@@ -1,6 +1,7 @@
 package com.unex.asee.ga02.beergo.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.unex.asee.ga02.beergo.model.User
@@ -11,4 +12,8 @@ interface UserDao {
     suspend fun findByName(first: String): User
     @Insert
     suspend fun insert(user: User): Long
+
+    @Delete
+    fun delete(user : User)
+
 }
