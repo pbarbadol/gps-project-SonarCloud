055d1fd45718f426982e9ba44103582fdd5ea663
alberto <adominguoii@alumnos.unex.es>
2023-12-09 00:50:56 +0100

    Desarrollando rama de refactorización 2

--- app/src/main/java/com/unex/asee/ga02/beergo/view/viewmodel/FavsViewModel.kt
+++ app/src/main/java/com/unex/asee/ga02/beergo/view/viewmodel/FavsViewModel.kt
@@ -1,23 +1,45 @@
 package com.unex.asee.ga02.beergo.view.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.unex.asee.ga02.beergo.BeerGoApplication
 import com.unex.asee.ga02.beergo.model.Beer
 import com.unex.asee.ga02.beergo.model.User
-import com.unex.asee.ga02.beergo.repository.BeerRepository
 import com.unex.asee.ga02.beergo.repository.FavRepository
-import com.unex.asee.ga02.beergo.repository.UserRepository
+import kotlinx.coroutines.launch
 
 class FavsViewModel(
-    private var beerRepository: BeerRepository,
-    private var userRepository: UserRepository,
     private var favRepository: FavRepository
 ): ViewModel() {
 
-    var user : User? = null
+    var user: User? = null
+        set(value) {
+            field = value
+        }
 
+    private val _spinner = MutableLiveData<Boolean>(false)
+    val spinner: LiveData<Boolean>
+        get() = _spinner
+    var beer: Beer? = null
+        set(value) {
+            field = value
+        }
+
+    private val _toast = MutableLiveData<String?>()
+    val toast: LiveData<String?>
+        get() = _toast
+
+    var favBeers: LiveData<List<Beer>> = MutableLiveData<List<Beer>>()
+    init{
+        favBeers = favRepository.loadFavs(user!!.userId)
+    }
+
+
+/*
     /**
      * Cambia el valor del LiveData de la cerveza seleccionada a null.
      * @return Método setSelecterBeer de beerRepository.
@@ -55,17 +77,28 @@ class FavsViewModel(
         return favRepository.loadFavs(userId)
     }
 
-    /**
-     * Borra una de las cervezas favoritas de un usuario
-     *
-     * @param userId el ID del usuario
-     * @param beerId el ID de la cerveza
-     * @return Método deleteFav de favRepository.
-     */
+
     suspend fun deleteFav(userId: Long, beerId: Long){
         return favRepository.deleteFav(userId, beerId)
+    }*/
+
+    fun loadFavourites(){
+        viewModelScope.launch {
+            _spinner.value = true
+            favBeers = favRepository.loadFavs(user!!.userId)
+            _spinner.value = false
+        }
+    }
+    fun onToastShown() {
+        _toast.value = null
     }
 
+    fun deleteBeer(beer: Beer) {
+        viewModelScope.launch {
+            favRepository.deleteFav(user!!.userId, beer.beerId)
+            _toast.value = "${beer.title} eliminada correctamente de favoritos"
+        }
+    }
     companion object {
         val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
@@ -77,8 +110,6 @@ class FavsViewModel(
                 val application =
                     checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
                 return FavsViewModel(
-                    (application as BeerGoApplication).appContainer.beerRepository,
-                    (application as BeerGoApplication).appContainer.userRepository,
                     (application as BeerGoApplication).appContainer.favRepository
                 ) as T
             }
